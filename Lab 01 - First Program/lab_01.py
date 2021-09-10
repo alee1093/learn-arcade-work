8eb5ccbade6cb70dea6ca07cc7368df2b56d4647
Ashley Lee <alee01@overlake.org>
2021-09-10 14:03:28 -0700

    Class Version

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,2 +1,4 @@
 print("Hello, my name is Ashley.")
 print("I go to Overlake.\nIt is located in Redmond, WA.")
+print("""Currently, I am writing a program for my class.
+It is called 'Learning to Program'""")
