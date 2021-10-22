e55f324545a96bfae9670ca9691347e7485103c3
Ashley Lee <alee01@overlake.org>
2021-10-21 20:01:28 -0700

    Print

--- Testing/print.py
+++ Testing/print.py
@@ -1,8 +1,1 @@
-def sum_list(list):
-    sum1 = 0
-    for i in list:
-        sum = i
-        sum1 += i
-    return sum1
-list = [45 ,2, 10, -5, 100]
-print(sum_list(list))
+print(" \" \nyay newline")
