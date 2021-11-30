f0893dc2e9a30d3d5b7ea046530fd192bbd90b6d
Ashley Lee <alee01@overlake.org>
2021-11-30 08:47:53 -0800

    Looping Problems

---
+++ Lab 05 - Loopy Lab/Advanced Looping.py
@@ -0,0 +1,44 @@
+for row in range(10):
+    print("* ", end="")
+print("")
+print("")
+
+for row in range(10):
+    print("* ", end="")
+print("")
+for row in range(5):
+    print("* ", end="")
+print("")
+for row in range(21):
+    print("* ", end="")
+print("")
+print("")
+
+for row in range(10):
+    for column in range(10):
+        print("* ", end="")
+    print("")
+print("")
+
+for row in range(10):
+    for column in range(5):
+        print("* ", end="")
+    print("")
+print("")
+
+for row in range(5):
+    for column in range(20):
+        print("* ", end="")
+    print("")
+print("")
+
+for row in range(10):
+    for column in range(10):
+        print(column, " ", end="")
+    print("")
+print("")
+
+for row in range(10):
+    for column in range(10):
+        print(row, " ", end="")
+    print("")
--- Lab 05 - Loopy Lab/lab_05.py
+++ Lab 05 - Loopy Lab/lab_05.py
@@ -3,16 +3,16 @@ import arcade
 
 def draw_section_outlines():
     # Draw squares on bottom
-    arcade.draw_rectangle_outline(150, 150, 300, 300, arcade.color.BLACK)
-    arcade.draw_rectangle_outline(450, 150, 300, 300, arcade.color.BLACK)
-    arcade.draw_rectangle_outline(750, 150, 300, 300, arcade.color.BLACK)
-    arcade.draw_rectangle_outline(1050, 150, 300, 300, arcade.color.BLACK)
+    arcade.draw_rectangle_outline(150, 150, 300, 300, arcade.color.WHITE)
+    arcade.draw_rectangle_outline(450, 150, 300, 300, arcade.color.WHITE)
+    arcade.draw_rectangle_outline(750, 150, 300, 300, arcade.color.WHITE)
+    arcade.draw_rectangle_outline(1050, 150, 300, 300, arcade.color.WHITE)
 
     # Draw squares on top
-    arcade.draw_rectangle_outline(150, 450, 300, 300, arcade.color.BLACK)
-    arcade.draw_rectangle_outline(450, 450, 300, 300, arcade.color.BLACK)
-    arcade.draw_rectangle_outline(750, 450, 300, 300, arcade.color.BLACK)
-    arcade.draw_rectangle_outline(1050, 450, 300, 300, arcade.color.BLACK)
+    arcade.draw_rectangle_outline(150, 450, 300, 300, arcade.color.WHITE)
+    arcade.draw_rectangle_outline(450, 450, 300, 300, arcade.color.WHITE)
+    arcade.draw_rectangle_outline(750, 450, 300, 300, arcade.color.WHITE)
+    arcade.draw_rectangle_outline(1050, 450, 300, 300, arcade.color.WHITE)
 
 
 def draw_section_1():
@@ -57,7 +57,7 @@ def draw_section_4():
             x = 905  # Instead of zero, calculate the proper x location using 'column'
             y = 5  # Instead of zero, calculate the proper y location using 'row'
             if row % 2 == 1 and column % 2 == 1:
-                arcade.draw_rectangle_filled(x + 10 * column, y + 10 * row, 5, 5, arcade.color.BLACK)
+                arcade.draw_rectangle_filled(x + 10 * column, y + 10 * row, 5, 5, arcade.color.WHITE)
             elif row % 2 == 0 and column % 2 == 0:
                 arcade.draw_rectangle_filled(x + 10 * column, y + 10 * row, 5, 5, arcade.color.WHITE)
             else:
@@ -100,7 +100,7 @@ def draw_section_8():
 def main():
     # Create a window
     arcade.open_window(1200, 600, "Lab 05 - Loopy Lab")
-    arcade.set_background_color(arcade.color.AIR_FORCE_BLUE)
+    arcade.set_background_color(arcade.color.GRAY)
 
     arcade.start_render()
 
